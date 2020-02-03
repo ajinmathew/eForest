d01e6a279f36ae181c38f35fb53094a7462f99bf
ajinmathew <mathewajin10@gmail.com>
2020-02-03 07:52:46 +0530

    home page created

--- app/src/main/java/com/android/cs/project/eforest/UserRegisteration2.java
+++ app/src/main/java/com/android/cs/project/eforest/UserRegisteration2.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Signup extends AppCompatActivity {
+public class UserRegisteration2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_user_registeration2);
     }
 }
---
+++ app/src/main/java/com/android/cs/project/eforest/ViewNews.java
@@ -0,0 +1,14 @@
+package com.android.cs.project.eforest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewNews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_news);
+    }
+}
