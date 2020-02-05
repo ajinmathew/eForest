671a4ba013348c7cb2a9190842ac65ed6897443f
ajinmathew <mathewajin10@gmail.com>
2020-02-05 17:33:40 +0530

    application ui created Sharedpreference implemented

---
+++ app/src/main/java/com/android/cs/project/eforest/HomeUi.java
@@ -0,0 +1,14 @@
+package com.android.cs.project.eforest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeUi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_ui);
+    }
+}
