e98b90b147fc00097db156ad11edb994ff674590
ajinmathew <mathewajin10@gmail.com>
2020-03-11 23:45:17 +0530

    Social Media Ui Created

---
+++ app/src/main/java/com/android/cs/project/eforest/SocialMedia.java
@@ -0,0 +1,14 @@
+package com.android.cs.project.eforest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SocialMedia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_media);
+    }
+}
