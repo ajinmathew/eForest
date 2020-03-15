168562a8caad61faa0eb4255c772c13fefcf9b11
ajinmathew <mathewajin10@gmail.com>
2020-03-15 23:22:57 +0530

    do recyclerview in applicationView Next...

---
+++ app/src/main/java/com/android/cs/project/eforest/ApplicationHome.java
@@ -0,0 +1,14 @@
+package com.android.cs.project.eforest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ApplicationHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_application_home);
+    }
+}
